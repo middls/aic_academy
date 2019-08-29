a5715163ce30a2afb206d2f66c891d380810e462
middls <middls647@gmail.com>
2019-08-29 22:39:02 +0300

    add js

---
+++ dev/assets/js/app.js
@@ -0,0 +1,29 @@
+// the callback function that will be fired
+// when the element apears in the viewport
+// because it fires on initialization
+// we have to check for change.isIntersecting parameter
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if(change.isIntersecting) {
+      change.target.classList.add('visible');
+    }
+  });
+}
+
+// list of options
+let options = {
+  threshold: [1]
+};
+
+// instantiate a new Intersection Observer
+let observer = new IntersectionObserver(onEntry, options);
+
+// list of paragraphs
+let elements = document.querySelectorAll('p');
+
+// loop through all elements
+// pass each element to observe method
+// ES2015 for-of loop can traverse through DOM Elements
+for (let elm of elements) {
+  observer.observe(elm);
+}
